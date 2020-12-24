644ef71dfc5dd13d5521b2bc1fba61c9c6d61b5e
Moin Khan <kmoin786@gmail.com>
2020-12-24 20:04:24 +0530

    convert App into a function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,13 @@
-import { Component } from 'react';
 import propTypes from 'prop-types';
 import './App.css';
 
-class App extends Component {
-  constructor({ title }) {
-    super({ title });
-    this.title = title;
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.title}
-        </header>
-      </div>
-    );
-  }
-}
+const App = ({ title }) => (
+  <div className="App">
+    <header className="App-header">
+      {title}
+    </header>
+  </div>
+);
 
 App.propTypes = {
   title: propTypes.string,
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="React Calculator" />
+    <App title="hello world" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
