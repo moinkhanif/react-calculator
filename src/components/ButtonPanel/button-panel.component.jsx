87ae265e19383e5d3e314fee2da706f91bd0cf6a
Moin Khan <kmoin786@gmail.com>
2020-12-24 18:45:40 +0530

    define and add groups to ButtonPanel

--- src/components/ButtonPanel/button-panel.component.jsx
+++ src/components/ButtonPanel/button-panel.component.jsx
@@ -1,3 +1,14 @@
-const ButtonPanel = () => <div>Hi</div>;
+const ButtonPanel = () => {
+  const groups = {
+    group1: ['AC', '+/-', '%', '÷'],
+    group2: ['7', '8', '9', 'X'],
+    group3: ['4', '5', '6', '-'],
+    group4: ['1', '2', '3', '+'],
+    group5: ['0', '.', '='],
+  };
+  return (
+    <div>Hi</div>
+  );
+};
 
 export default ButtonPanel;
