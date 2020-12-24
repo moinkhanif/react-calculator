4eb16092dede4e1db6267017ad19615c0d055bc2
Moin Khan <kmoin786@gmail.com>
2020-12-24 14:36:28 +0530

    require title prop for App and fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import { Component } from 'react';
+import propTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor({ title }) {
+    super({ title });
+    this.title = title;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+            and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.title}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  title: propTypes.string,
+};
+
+App.defaultProps = {
+  title: 'Calculator',
+};
 
 export default App;
