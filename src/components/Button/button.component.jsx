d8c265a3b87e61df46310395eb9f83068a8e0c04
Moin Khan <kmoin786@gmail.com>
2020-12-29 23:26:07 +0530

    add styles to button component

--- src/components/Button/button.component.jsx
+++ src/components/Button/button.component.jsx
@@ -4,9 +4,11 @@ import './button.styles.css';
 const handleClick = clickHandler => clickHandler;
 
 const Button = props => {
-  const { name, title, clickHandler } = props;
+  const {
+    name, title, clickHandler, color, wide,
+  } = props;
   return (
-    <button type="button" onClick={handleClick(clickHandler)} className="calculator-key" title={title}>{ name }</button>
+    <button type="button" onClick={handleClick(clickHandler)} className={`calculator-key ${wide} ${color}`} title={title}>{ name }</button>
   );
 };
 
@@ -14,11 +16,15 @@ Button.propTypes = {
   name: propTypes.string,
   title: propTypes.string,
   clickHandler: propTypes.func.isRequired,
+  color: propTypes.string,
+  wide: propTypes.bool,
 };
 
 Button.defaultProps = {
   name: '',
   title: undefined,
+  color: 'orange',
+  wide: false,
 };
 
 export default Button;
