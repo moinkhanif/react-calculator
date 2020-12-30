f59e6a7912a1018e9edcf12e65c36d353d70200e
Moin Khan <kmoin786@gmail.com>
2020-12-30 14:05:58 +0530

    add and style header container

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,3 +1,17 @@
-const Header = () => <div>header</div>;
+import { Link } from 'react-router-dom';
+import './Header.styles.css';
+
+const Header = () => (
+  <header>
+    <Link className="logo" to="/">Math Magicians</Link>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/quote">Quote</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
