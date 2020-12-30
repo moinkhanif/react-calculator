bb5be5d157e27acd5a5c71326724f8ea6e249e8f
Moin Khan <kmoin786@gmail.com>
2020-12-30 15:43:24 +0530

    add and style quote container

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Calculator from './container/Calculator/Calculator';
 import Header from './container/Header/Header';
 import Home from './container/Home/Home';
+import Quote from './container/Quote/Quote';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Routes = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/calculator" component={Calculator} />
+        <Route path="/quote" component={Quote} />
       </Switch>
     </main>
   </BrowserRouter>
---
+++ src/container/Quote/Quote.jsx
@@ -0,0 +1,14 @@
+import QUOTES from './Quotes-data';
+import './Quote.styles.css';
+
+const Quote = () => {
+  const [quote, author] = QUOTES[Math.floor(Math.random() * (QUOTES.length))].split('–');
+  return (
+    <blockquote id="quote">
+      {quote}
+      <span>{`- ${author}`}</span>
+    </blockquote>
+  );
+};
+
+export default Quote;
