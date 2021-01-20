f0841b9e453bcc99db7f9a4bd6929f91e6ff4104
Moin Khan <kmoin786@gmail.com>
2021-01-20 16:23:21 +0530

    add app snapshot test

--- src/container/App/App.test.js
+++ src/container/App/App.test.js
@@ -1,6 +1,7 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import renderer from 'react-test-renderer';
+import App from './App';
 
 test('renders learn react link', () => {
-
+  const result = renderer.create(<App />).toJSON();
+  expect(result).toMatchSnapshot();
 });
